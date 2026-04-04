28aca010677b5a56f4239b29165e8866fedf422f
gulin225 <ggulin225@gmail.com>
2026-04-04 17:40:46 -0500

    Worked on hashtable, not working yet

--- src/ds.c
+++ src/ds.c
@@ -61,63 +61,120 @@ int count_nodes(Node *root) {
 //Dynamic array that doubles in capacity when full. After this, the stack unit tests should pass.
 /* TODO 5 */
 void fs_init(FrameStack *s) {
-    s->frames = NULL;
+
     s->capacity = 1;
     s->size = 0;
+    Frame* dummy = malloc(s->capacity * sizeof(Frame));
+    if(dummy == NULL){
+        return;
+    }
+    s->frames = dummy;
 }
 
 /* TODO 6 */
 void fs_push(FrameStack *s, Node *node, int answeredYes) {
-    if(s->capacity >= s->size){
+    if(s->capacity <= s->size){
         s->capacity = s->capacity * 2;
-        s->frames= realloc(s->frames, sizeof(Frame*) * s->capacity);
+        Frame* dummy = realloc(s->frames, sizeof(Frame) * s->capacity);
+        if(dummy == NULL){
+            return;
+        }
+        else{
+            s->frames = dummy;
+        }
     }
 
-    Frame* dummy = malloc(sizeof(Frame));   
 
+
+    s->frames[s->size].node = node; 
+    s->frames[s->size].answeredYes = answeredYes;
     s->size = s->size + 1;
 
 }
 
 /* TODO 7 */
+//if we get errors for empty, come back
 Frame fs_pop(FrameStack *s) {
-    Frame* dummy = s->frames;
+    Frame dummy = s->frames[s->size - 1]; 
+    s->size = s->size - 1;
+
+    return dummy;
     
 }
 
 /* TODO 8 */
 int fs_empty(FrameStack *s) {
-    return 1;
+    if(s->size == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 /* TODO 9 */
 void fs_free(FrameStack *s) {
+    free(s->frames);
+    s->size = 0;
+    s->capacity = 0;
+    s->frames = NULL;
 }
 
 
 /* ====== EditStack  (dynamic array, undo/redo) =================== */
 
 /* TODO 10 */
+// Structurally identical to FrameStack but stores Edit structs. The pattern is the same.
+
 void es_init(EditStack *s) {
+    s->capacity = 1;
+    s->size = 0;
+    Edit* dummy = malloc(s->capacity * sizeof(Edit));
+    if(dummy == NULL){
+        return;
+    }
+    s->edits = dummy;
 }
 
 /* TODO 11 */
 void es_push(EditStack *s, Edit e) {
+     if(s->capacity <= s->size){
+        s->capacity = s->capacity * 2;
+        Edit* dummy = realloc(s->edits, sizeof(Edit) * s->capacity);
+        if(dummy == NULL){
+            return;
+        }
+        else{
+            s->edits = dummy;
+        }
+    }
+
+
+
+    s->edits[s->size] = e;
+    s->size = s->size + 1;
+
 }
 
 /* TODO 12 */
+
 Edit es_pop(EditStack *s) {
-    Edit dummy = {0};
+    Edit dummy = s->edits[s->size -1];
+    s->size = s->size -1;
     return dummy;
 }
 
 /* TODO 13 */
 int es_empty(EditStack *s) {
+   if(s->size == 0){
     return 1;
+   }
+   return 0;
 }
 
 /* TODO 14 */
 void es_clear(EditStack *s) {
+    s->size = 0;
 }
 
 /* provided -- do not modify */
@@ -132,27 +189,82 @@ void free_edit_stack(EditStack *s) { es_free(s); }
 
 
 /* ====== Queue  (linked list, BFS) ============================== */
-
+//Linked-list queue. Pay close attention to what must happen when the 
+//last element is dequeued — this is the most common source of crashes in this section.
 /* TODO 15 */
 void q_init(Queue *q) {
+    q->size = 0;
+    q->front = NULL;
+    q->rear = NULL;
 }
 
 /* TODO 16 */
 void q_enqueue(Queue *q, Node *node, int id) {
+    QueueNode* dummy= malloc(sizeof(QueueNode));
+    if(dummy == NULL){
+        return;
+    }
+    dummy->treeNode = node;
+    dummy->id = id;
+    dummy->next = NULL;
+    if(q->front == NULL){
+        q->front = dummy;
+        q->rear = dummy;
+        q->size = q->size + 1;
+        return;
+    }
+    q->rear->next = dummy;
+    q->rear = dummy;
+    q->size = q->size + 1;
 }
 
 /* TODO 17 */
 int q_dequeue(Queue *q, Node **node, int *id) {
-    return 0;
+    if(q->size == 0){
+        return 0;
+    }
+
+    QueueNode* dummy= q->front;
+    *node = (dummy->treeNode);
+    *id = (dummy->id);
+    if(q->size == 1){
+        q->front = NULL;
+        q->rear = NULL;
+        q->size = q->size - 1;
+        free(dummy);
+        return 1;
+    }
+    q->size = q->size - 1;
+
+    q->front = dummy->next;
+    
+
+    free(dummy);
+    return 1;
+    
 }
 
 /* TODO 18 */
 int q_empty(Queue *q) {
-    return 1;
+    if(q->size == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 /* TODO 19 */
 void q_free(Queue *q) {
+    QueueNode* curr = q->front;
+    while(curr != NULL){
+        QueueNode* temp = curr->next;
+        free(curr);
+        curr = temp;
+    }
+    q->front = NULL;
+    q->rear = NULL;
+    q->size = 0;
 }
 
 
@@ -167,21 +279,118 @@ void q_free(Queue *q) {
  */
 char *canonicalize(const char *s) {
     if (s == NULL) return strdup("");
-    return NULL;
+
+    uint32_t len = strlen(s);
+    char* mine = calloc(len + 1, sizeof(char));
+    int m = 0;
+
+    if(mine == NULL){
+        return NULL;
+    }
+
+    for(uint32_t i = 0; i < len; i++){
+        if(isalpha(s[i])){
+            mine[m] = tolower(s[i]);
+            m++;
+        }
+        else if(s[i] == ' '){
+            mine[m] = '_';
+            m++;
+        }
+        
+    }
+
+    return mine;
+
 }
 
 /* TODO 21  (djb2: hash = hash*33 + c, seed 5381) */
 unsigned h_hash(const char *s) {
-    return 0;
+    unsigned hash = 5381; // taken from google
+
+    uint32_t length = strlen(s);
+
+    for(uint32_t i = 0; i < length; i++){
+        hash = (hash*33) + s[i];
+    }
+
+    return hash;
 }
 
 /* TODO 22 */
 void h_init(Hash *h, int nbuckets) {
+    if (h == NULL) return;
+
+    h->nbuckets = nbuckets;
+    h->size = 0;
+    
+    Entry** dummy = calloc(nbuckets, sizeof(Entry*)); 
+    if(dummy == NULL){
+        h->nbuckets = 0;
+        return;
+    }
+    h->buckets = dummy;
+
 }
 
 /* TODO 23 */
 int h_put(Hash *h, const char *key, int solutionId) {
-    return 0;
+    unsigned hashNum = hash(key);
+
+    unsigned index = hashNum % h->nbuckets;
+    uint32_t i = 0;
+
+    if(h->buckets[index] == NULL){
+        Entry* dummy = malloc(sizeof(Entry));
+        if(dummy == NULL){
+            return 0;
+        }
+        dummy->key = key;
+        dummy->next =NULL;
+
+        dummy->vals.capacity = 1;
+        dummy->vals.count = 1;
+        int* temp = malloc(dummy->vals.capacity * sizeof(int));
+        if(temp == NULL){
+            return 0;
+        }
+    
+        temp[0] = solutionId;
+        dummy->vals.ids = temp;
+
+        dummy->next = NULL;
+        return 1;
+    }
+    else{
+        Entry* current = h->buckets[index];
+        Entry* prev = h->buckets[index];
+        while(current != NULL){
+            prev = current;
+            
+            if(strcmp(current->key,key) == 0){
+                if(current->vals.count >= current->vals.capacity){
+                    current->vals.capacity*=2;
+                    int* tempCap = realloc(current->vals.ids, sizeof(int) * current->vals.capacity);
+                    if(tempCap == NULL){
+                        return 0;
+                    }
+
+                    current->vals.count = current->vals.count + 1;
+                    current->vals.ids = tempCap;
+                    current->vals.ids[current->vals.count - 1] = solutionId; //setting the value at the index = to the solutionid
+                    return 1;
+                }
+            }
+            
+            current = current->next;
+            
+        }
+
+    
+
+    }
+
+
 }
 
 /* TODO 24 */
